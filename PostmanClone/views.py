4eed511cc3fa341e645e01d4368d64d0385c0c88
Siegel <csiegel@dmu.edu>
2018-10-20 15:23:51 -0500

    when a new call is saved now it does so under the current session collection!!!  still lots of testing and trying to break thigns to be done.

--- PostmanClone/views.py
+++ PostmanClone/views.py
@@ -11,10 +11,6 @@ from django.core.mail import send_mail
 # Create your views here.
 def index(request):
     context = {}
-    if 'collection' not in request.session:
-         print("no collection chosen!!")
-    else:
-        print(request.session['collection'])
     if request.method == "POST":
         baseURL = request.POST['baseURL']
         httpMethod = request.POST['httpMethod']
@@ -33,19 +29,22 @@ def index(request):
     user_collections = [i for i in Collection.objects.filter(user = current_user.id)]
     #hash table to store calls and connect them to a certain collection!!!
     user_calls = []
+    current_collection_name = None
     if 'collection' not in request.session:
-        user_calls.append("no saved calls right now sorry")
+        pass
     else:
         #this is definitely an unnecessary DB call, try to eliminate this if possible...
+        current_collection_name = request.session['collection']
         current_session_collection = get_object_or_404(Collection, name = request.session['collection'])
         user_calls.append(apiCall.objects.filter(collection = current_session_collection))
 
-    context.update({'user_collections': user_collections, 'user_calls': user_calls})
+    context.update({'user_collections': user_collections, 'user_calls': user_calls, 'current_collection_name': current_collection_name})
     return render(request, 'PostmanClone/index.html', context)
 
 def submit(request):
     if 'saveButton' in request.POST:
         current_user = request.user
+        current_collection = get_object_or_404(Collection, name=request.session['collection'])
         headers = ast.literal_eval((request.POST['headers']))
         headersList = []
         for key, value in headers.items():
@@ -54,7 +53,7 @@ def submit(request):
 
         #make a separate method that you pass POST into sometime to clean up this code!!  it can return your entire context
         new_api_call = apiCall()
-        new_api_call.base_url, new_api_call.headersa1, new_api_call.headersb1, new_api_call.headersa2, new_api_call.headersb2, new_api_call.headersa3, new_api_call.headersb3, new_api_call.httpMethod, new_api_call.user, new_api_call.name = request.POST['baseURL'], headersList[0], headersList[1], headersList[2], headersList[3], headersList[4], headersList[5], request.POST['httpMethod'], current_user, request.POST['api_call_name']
+        new_api_call.base_url, new_api_call.headersa1, new_api_call.headersb1, new_api_call.headersa2, new_api_call.headersb2, new_api_call.headersa3, new_api_call.headersb3, new_api_call.httpMethod, new_api_call.collection, new_api_call.name = request.POST['baseURL'], headersList[0], headersList[1], headersList[2], headersList[3], headersList[4], headersList[5], request.POST['httpMethod'], current_collection, request.POST['api_call_name']
         new_api_call.save()
         return redirect('index')
 
